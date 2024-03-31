51d9ad3a1847b03079c10c158d94cc4ed34321d8
trishaDas13 <trisha.das1308@gmail.com>
2024-03-31 12:08:19 +0530

    active tab highlighted

--- src/components/asidebar/Asidebar.jsx
+++ src/components/asidebar/Asidebar.jsx
@@ -9,7 +9,7 @@ const Asidebar = () => {
   return (
     <>
     {
-      isShow && <aside classname={isShow ? `bar_active` : ``}>
+      isShow && <aside className='show'>
       <div className="above">
         <h1>Carbon Cell</h1>
         <i class="fa-regular fa-circle-xmark" onClick={() => setIsShow(false)}></i>
